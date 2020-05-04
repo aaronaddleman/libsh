53d83c1b33085a22ba04319c0bb23f56d8c5eb2d
Aaron Addleman <aaronaddleman@gmail.com>
2020-05-04 14:21:50 -0700

    Modified files search for .bash.d
    - If applied, this commit will...
    Fixes loading of files and prevents ugly error
    
    - Explain why this change is being made...
    Before, the sh would complain if there were no files. Now it will not.
    
    - Provide links to any relevant tickets, articles, or other resources...

--- bash.env.sh
+++ bash.env.sh
@@ -1,2 +1,8 @@
 # load any additional settings
-[ -d $HOME/.bash.d ] && source $HOME/.bash.d/*.env.sh
+#[ -d $HOME/.bash.d ] && ls $HOME/.bash.d/*.env.sh 1> /dev/null 2>&1
+
+#[ "$?" == "0" ] && source $HOME/.bash.d/*.env.sh
+
+local num_of_env_sh=$(find $HOME/.bash.d -maxdepth 1 -type f  -name '*.env.sh' | wc -l)
+libsh__debug "INFO Found ${num_of_env_sh} files matching $HOME/.bash.d/*.env.sh ."
+[ "${num_of_env_sh}" -gt 0 ] && source $HOME/.bash.d/*.env.sh
