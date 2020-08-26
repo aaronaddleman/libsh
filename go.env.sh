2a47b76ce0ffccba83d05e26dc3b34037fd87ce2
Aaron Addleman <aaronaddleman@gmail.com>
2020-08-26 11:56:29 -0700

    add goenv manager

--- go.env.sh
+++ go.env.sh
@@ -1,3 +1,8 @@
+# GOENV
+[ -d $HOME/.goenv ] && export GOENV_ROOT="$HOME/.goenv"
+[ -d $HOME/.goenv ] && libsh__add_path "pre" "$GOENV_ROOT/bin"
+[ -z $GOENV_ROOT ] || eval "$(goenv init -)"
+
 # GOPATH
 [ -d $HOME/go ] && export GOPATH="$HOME/go" || libsh__debug "x..could not find $HOME/go"
 [ -d $HOME/go/bin ] && libsh__add_path "post" "$HOME/go/bin"
