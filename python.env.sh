93cac19db2e7b1202cc02c4c6c8ced79fb6bd6c2
Aaron Addleman <aaronaddleman@gmail.com>
2020-10-28 08:58:50 -0700

    Disabling workon
    I tried workon and it did not work for me. Thus I am commenting it out
    as a reminder for myself in the future

--- python.env.sh
+++ python.env.sh
@@ -1,4 +1,5 @@
+## disabled until I find a reason to use "workon" because its confusing
 # set your python workon home or use default
-[ -n "$LIBSH_PYTHON_WORKON_HOME" ] && \
-    export WORKON_HOME=${${LIBSH_PYTHON_WORKON_HOME}-$HOME/.virtualenvs} && \
-    export PROJECT_HOME=${HOME}/src
+# [ -n "$LIBSH_PYTHON_WORKON_HOME" ] && \
+#     export WORKON_HOME=${${LIBSH_PYTHON_WORKON_HOME}-$HOME/.virtualenvs} && \
+#     export PROJECT_HOME=${LIBSH_PYTHON_PROJECT_HOME-${HOME}/src}
