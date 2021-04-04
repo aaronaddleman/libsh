a7339b42c3e804cb0eca0ea9a2c8a1b6498b1a63
Aaron Addleman <aaronaddleman@gmail.com>
2021-04-04 11:11:49 -0700

    adding .local/bin for utils

--- 1password.sh
+++ 1password.sh
@@ -1,5 +1,5 @@
 1password_install_cli() {
     [[ "$(uname)" == "Darwin" ]] && brew install 1password-cli && libsh__exit_with_message "Installed" "1password-cli"
     [[ "$(uname)" == "Linux" ]] && export LIBSH_OSLINUX=t
-    [[ "${LIBSH_OSLINUX}" == "t" ]] && wget -O /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_386_v1.8.0.zip && unzip /tmp/op.zip -d /usr/local/bin
+    [[ "${LIBSH_OSLINUX}" == "t" ]] && wget -O /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_386_v1.8.0.zip && mkdir -p $HOME/.local/bin && unzip /tmp/op.zip -d $HOME/.local/bin
 }
