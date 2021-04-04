7ce4ab85dc83a8474ec53bfb459c722e0487224d
Aaron Addleman <aaronaddleman@gmail.com>
2021-04-04 11:05:55 -0700

    add install for linux

--- 1password.sh
+++ 1password.sh
@@ -1,3 +1,5 @@
 1password_install_cli() {
-    brew install 1password-cli
+    [[ "$(uname)" == "Darwin" ]] && brew install 1password-cli && libsh__exit_with_message "Installed" "1password-cli"
+    [[ "$(uname)" == "Linux" ]] && export LIBSH_OSLINUX=t
+    [[ "${LIBSH_OSLINUX}" == "t" ]] && wget -O /tmp/op.zip https://cache.agilebits.com/dist/1P/op/pkg/v1.8.0/op_linux_386_v1.8.0.zip && unzip /tmp/op.zip -d /usr/local/bin
 }
