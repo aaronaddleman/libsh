3ad8182c1d61f5c136e40885082739f505c2ee06
Aaron Addleman <aaronaddleman@gmail.com>
2021-07-18 22:25:25 -0700

    ya.. pyenv should init should be here

--- pyenv.sh
+++ pyenv.sh
@@ -1,7 +1,7 @@
 if [ -d $HOME/.pyenv ]; then
     if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
     command -v pyenv > /dev/null
-    eval "$(pyenv init -)"
+    eval "$(pyenv init --path)"
     libsh__debug "FN +Loaded pyenv init"
 fi
 
