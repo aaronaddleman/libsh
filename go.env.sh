b06c3d750d994a5366429f2687a9ee61f5d448ae
Aaron Addleman <aaronaddleman@gmail.com>
2022-05-10 22:03:20 -0700

    try fixing first warning from shellcheck

--- go.env.sh
+++ go.env.sh
@@ -12,7 +12,7 @@
 # GOPATH
 ## this will not conflict with goenv because the destination for 
 ## for goenv is $HOME/go/VER/bin
-[ -d $HOME/go -a ! -d $HOME/.goenv ] && export GOPATH="$HOME/go" || libsh__debug "x..could not find $HOME/go"
+[ -d $HOME/go && ! -d $HOME/.goenv ] && export GOPATH="$HOME/go" || libsh__debug "x..could not find $HOME/go"
 [ -d $HOME/go/bin ] && libsh__add_path "post" "$HOME/go/bin"
 
 # GOROOT
